1eb661a856359975f5365f364cf9688c25ac93dc
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2025-02-28 11:39:04 -0500

    feat: :sparkles: Add body for functions and methods in parsed_data

--- src/code_parser/parse_ast.py
+++ src/code_parser/parse_ast.py
@@ -31,58 +31,85 @@ def analyze_tree(tree, extension):
         def traverse(node, parent_class=None):
             if extension == ".py":
                 if node.type == "function_definition":
-                    # child_by_field_name("name") es un método de los nodos en Tree-sitter. 
-                    # Se está obteniendo el nombre de un método o clase dentro de un nodo
-                    # En Tree-sitter, text devuelve un objeto en formato bytes, text.decode() Convierte los bytes en una cadena (str).
-
                     func_name = node.child_by_field_name("name").text.decode()
+                    body_node = node.child_by_field_name("body")
+                    func_body = body_node.text.decode() if body_node else "No body"
+
                     if parent_class:
-                        # Si estamos dentro de una clase, guardamos el método en ella
-                        parsed_data["classes"][parent_class]["methods"][func_name] = "No docstring"
+                        parsed_data["classes"][parent_class]["methods"][func_name] = func_body
                     else:
-                        # Si no hay clase padre, es una función global
-                        parsed_data["functions"][func_name] = "No docstring"
-                
+                        parsed_data["functions"][func_name] = func_body
+
                 elif node.type == "class_definition":
                     class_name = node.child_by_field_name("name").text.decode()
                     parsed_data["classes"][class_name] = {"docstring": "No docstring", "methods": {}}
-                
-                    # Recorrer los hijos de la clase y detectar métodos
+
                     for child in node.children:
                         traverse(child, parent_class=class_name)
-                        
+
                 elif node.type in ["import_statement", "import_from_statement"]:
                     parsed_data["imports"].append(node.text.decode())
 
-            elif extension in [".js", ".jsx", ".cjs"]:
+            elif extension in [".js", ".cjs"]:
                 if node.type == "function_declaration":
                     func_name = node.child_by_field_name("name").text.decode()
-                    parsed_data["functions"][func_name] = "No docstring"
-                
+                    body_node = node.child_by_field_name("body")
+                    func_body = body_node.text.decode() if body_node else "No body"
+                    parsed_data["functions"][func_name] = func_body
+
                 elif node.type == "class_declaration":
                     class_name = node.child_by_field_name("name").text.decode()
                     parsed_data["classes"][class_name] = {"docstring": "No docstring", "methods": {}}
-                
+
                 elif node.type == "method_definition" and parent_class:
                     method_name = node.child_by_field_name("name").text.decode()
+                    body_node = node.child_by_field_name("body")
+                    method_body = body_node.text.decode() if body_node else "No body"
+
                     if parent_class not in parsed_data["classes"]:
                         parsed_data["classes"][parent_class] = {"docstring": "No docstring", "methods": {}}
-                    parsed_data["classes"][parent_class]["methods"][method_name] = "No docstring"
-                
+
+                    parsed_data["classes"][parent_class]["methods"][method_name] = method_body
+
                 elif node.type == "import_statement":
-                                parsed_data["imports"].append(node.text.decode())
+                    parsed_data["imports"].append(node.text.decode())
+            
+            elif extension in [".jsx"]:
+                if node.type == "function_declaration":
+                    func_name = node.child_by_field_name("name").text.decode()
+                    parsed_data["functions"][func_name] = ''
+
+                elif node.type == "class_declaration":
+                    class_name = node.child_by_field_name("name").text.decode()
+                    parsed_data["classes"][class_name] = {"docstring": "No docstring", "methods": {}}
+
+                elif node.type == "method_definition" and parent_class:
+                    method_name = node.child_by_field_name("name").text.decode()
+
+                    if parent_class not in parsed_data["classes"]:
+                        parsed_data["classes"][parent_class] = {"docstring": "No docstring", "methods": {}}
+
+                    parsed_data["classes"][parent_class]["methods"][method_name] = ''
+
+                elif node.type == "import_statement":
+                    parsed_data["imports"].append(node.text.decode())
+
 
             elif extension == ".java":
                 if node.type == "class_declaration":
                     class_name = node.child_by_field_name("name").text.decode()
                     parsed_data["classes"][class_name] = {"docstring": "No docstring", "methods": {}}
-                
+
                 elif node.type == "method_declaration" and parent_class:
                     method_name = node.child_by_field_name("name").text.decode()
+                    body_node = node.child_by_field_name("body")
+                    method_body = body_node.text.decode() if body_node else "No body"
+
                     if parent_class not in parsed_data["classes"]:
                         parsed_data["classes"][parent_class] = {"docstring": "No docstring", "methods": {}}
-                    parsed_data["classes"][parent_class]["methods"][method_name] = "No docstring"
-                
+
+                    parsed_data["classes"][parent_class]["methods"][method_name] = method_body
+
                 elif node.type == "import_declaration":
                     parsed_data["imports"].append(node.text.decode())
 
