6b3690fbdbdbafb3044af04854260c8774179aee
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2025-03-04 11:24:01 -0500

    fix: :bug: Fix a bug with the extract code process

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter
+import os
+import sys
 from models import DocumentationRequest, DocumentationResponse
-from code_parser.parse_ast import ast_analysis
+from src.code_parser.extract_code import extract_code
+from src.code_parser.parse_ast import ast_analysis
+from src.code_parser.preprocess import clean_code
 from ai_processing.inferences import generate_doccumentation
 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 # Se crea un router para agrupar las rutas
 router = APIRouter() 
 
 @router.post("/documentation")
 async def generate_docs(request: DocumentationRequest):
-    extracted_code = ast_analysis(request.repo_path)
-    documentation = generate_doccumentation(extracted_code)
+    extracted_code = extract_code(request.repo_path)
+    cleaned_code = clean_code(extracted_code)
+    code = ast_analysis(cleaned_code)
+    documentation = generate_doccumentation(code)
     return DocumentationResponse(repo_path = request.repo_path, generate_docs=documentation)
